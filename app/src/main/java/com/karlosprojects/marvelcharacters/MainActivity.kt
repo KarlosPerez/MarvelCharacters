af23fe5addf9114e67b1ef20c6865a936f1e7d10
Carlos Andres <legomarth@hotmail.com>
2022-08-12 02:06:59 -0500

    fix: splash screen issue

--- app/src/main/java/com/karlosprojects/marvelcharacters/MainActivity.kt
+++ app/src/main/java/com/karlosprojects/marvelcharacters/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
